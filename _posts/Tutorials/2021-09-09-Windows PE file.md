2dbb3b7b3b251f08e143eb4c8a0b5c45e8c40b70
shadyshahin <shady.shahin7@icloud.com>
2021-09-15 19:26:41 +0200

    initial

---
+++ _posts/Tutorials/2021-09-09-Windows PE file.md
@@ -0,0 +1,12 @@
+---
+title: "Windows PE file"
+classes: wide
+header:
+  teaser: /assets/images/articles/windowspe/windows.png
+ribbon: DodgerBlue
+description: "An inside look at pe file."
+categories:
+  - Articles
+toc: true
+---
+to be cont...
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Windows PE file",
+        "excerpt":"to be cont…  ","categories": ["Articles"],
+        "tags": [],
+        "url": "http://localhost:4000/articles/Windows-PE-file/",
+        "teaser": "http://localhost:4000/assets/images/articles/windowspe/windows.png"
+      }]
